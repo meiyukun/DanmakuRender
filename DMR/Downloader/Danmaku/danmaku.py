11e0f18eefb1f9912d839f04e903d978f1798f25
qingyan <534590526@qq.com>
2025-04-18 11:33:12 +0800

    添加emoji替换表情

--- DMR/Downloader/Danmaku/danmaku.py
+++ DMR/Downloader/Danmaku/danmaku.py
@@ -8,20 +8,22 @@ import platform
 from datetime import datetime
 from os.path import *
 
+from DMR.Downloader.Danmaku.asshandle import replace_emoji_in_text
 from DMR.LiveAPI.danmaku import DanmakuClient
 from DMR.utils import SimpleDanmaku
 
 __all__ = ['DanmakuDownloader']
 
+
 class DanmakuDownloader():
     def __init__(self,
-                 url:str,
-                 output:str,
-                 segment:float,
-                 dm_format:str,
-                 dm_filter:dict=None,
-                 dm_stream_option:dict={},
-                 advanced_dm_args:dict={},
+                 url: str,
+                 output: str,
+                 segment: float,
+                 dm_format: str,
+                 dm_filter: dict = None,
+                 dm_stream_option: dict = {},
+                 advanced_dm_args: dict = {},
                  **kwargs) -> None:
         self.stoped = False
 
@@ -53,7 +55,7 @@ class DanmakuDownloader():
         except Exception as e:
             self.logger.warn(f'弹幕屏蔽词{keywords_filter}设置错误:{e}，此功能将不会生效.')
             self.dm_filter['keywords'] = []
-        
+
         try:
             username_filter = dm_filter['username']
             if not username_filter:
@@ -69,13 +71,13 @@ class DanmakuDownloader():
         except Exception as e:
             self.logger.warning(f'用户屏蔽{username_filter}设置错误:{e}，此功能将不会生效.')
             self.dm_filter['username'] = []
-        
+
         self.kwargs = kwargs
         self.part = 0
 
         if platform.system() == 'Windows':
             asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-        
+
         if dm_format == 'ass':
             from .asswriter import AssWriter
             self.dmwriter = AssWriter(**self.kwargs)
@@ -88,26 +90,26 @@ class DanmakuDownloader():
     def start(self, self_segment=False):
         self.start_time = datetime.now().timestamp()
         self.part_start_time = self.start_time
-        self.dm_file = self.output.replace(f'%03d','%03d'%self.part)
+        self.dm_file = self.output.replace(f'%03d', '%03d' % self.part)
         self.dmwriter.open(self.dm_file)
 
         def monitor():
             while not self.stoped:
                 self.split()
                 time.sleep(self.segment)
-        
+
         if self_segment:
-            self.monitor = threading.Thread(target=monitor,daemon=True)
+            self.monitor = threading.Thread(target=monitor, daemon=True)
             self.monitor.start()
-        
+
         return self.start_dmc()
-    
-    def split(self, filename:str=None):
+
+    def split(self, filename: str = None):
         self.part += 1
         self.part_start_time = datetime.now().timestamp()
         old_dm_file = self.dm_file
         if not self.stoped:
-            new_dm_file = self.output.replace(f'%03d','%03d'%self.part)
+            new_dm_file = self.output.replace(f'%03d', '%03d' % self.part)
             self.logger.debug(f'New DMfile: {new_dm_file}')
             self.dmwriter.open(new_dm_file)
             self.dm_file = new_dm_file
@@ -117,27 +119,27 @@ class DanmakuDownloader():
             except Exception as e:
                 self.logger.error(f'弹幕 {old_dm_file} 分段失败: {e}.')
 
-    def dm_available(self, dm:SimpleDanmaku) -> bool:
+    def dm_available(self, dm: SimpleDanmaku) -> bool:
         if dm.time < 0 \
                 or not dm.content \
                 or not dm.uname \
                 or dm.dtype != 'danmaku':
             return False
-        
+
         for keyword in self.dm_filter['keywords']:
             if keyword.search(dm.content):
                 return False
-        
+
         for username in self.dm_filter['username']:
             if username.fullmatch(dm.uname):
                 return False
-        
+
         return True
-    
+
     def start_dmc(self):
-        async def danmu_monitor(url:str=None):
+        async def danmu_monitor(url: str = None):
             if not url:
-                url = self.url  
+                url = self.url
             q = asyncio.Queue()
 
             async def dmc_task():
@@ -150,7 +152,7 @@ class DanmakuDownloader():
                 except Exception as e:
                     await dmc.stop()
                     self.logger.exception(e)
-                
+
             task = asyncio.create_task(dmc_task())
             last_dm_time = datetime.now().timestamp()
             retry = 0
@@ -167,12 +169,13 @@ class DanmakuDownloader():
                     )
                     if self.dm_available(danmu):
                         retry = 0
+                        danmu.content = replace_emoji_in_text(danmu.content)
                         if self.dmwriter.add(danmu):
                             last_dm_time = datetime.now().timestamp()
                     continue
                 except asyncio.QueueEmpty:
                     pass
-                
+
                 if task.done():
                     self.logger.error(f'{self.url} 弹幕下载线程异常退出，正在重试...')
                     try:
@@ -182,17 +185,17 @@ class DanmakuDownloader():
                     task.cancel()
                     retry += 1
                     last_dm_time = datetime.now().timestamp()
-                    await asyncio.sleep(min(15*retry,60))
+                    await asyncio.sleep(min(15 * retry, 60))
                     task = asyncio.create_task(dmc_task())
                     continue
 
-                if self.dm_auto_restart and datetime.now().timestamp()-last_dm_time>self.dm_auto_restart:
+                if self.dm_auto_restart and datetime.now().timestamp() - last_dm_time > self.dm_auto_restart:
                     self.logger.error(f'{self.url} 获取弹幕超时，正在重试...')
                     task.cancel()
                     last_dm_time = datetime.now().timestamp()
                     task = asyncio.create_task(dmc_task())
                     continue
-                
+
                 await asyncio.sleep(0.1)
             task.cancel()
             try:
